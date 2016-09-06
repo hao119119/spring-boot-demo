17d8db58b56bc60f9e81557710fc2e04e3a2ce76
cc <hao119119@qq.com>
2016-09-06 14:14:30 +0800

    测试

---
+++ src/main/java/com/inspur/bigdata/service/Task.java
@@ -0,0 +1,51 @@
+/**
+ * @(#)Task.java V1.2.0 16-9-6 
+ * Copyright (c) 2014-2016 The inspur Software Foundation.All rights reserved 
+ */
+package com.inspur.bigdata.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.Future;
+
+/**
+ * @author Chen Hao
+ * @version V1.2.0 16-9-6
+ */
+@Component
+public class Task {
+    public static Random random =new Random();
+
+    @Async
+    public Future<String> doTaskOne() throws Exception {
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务一完成");
+    }
+
+    @Async
+    public Future<String> doTaskTwo() throws Exception {
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务二完成");
+    }
+
+    @Async
+    public Future<String> doTaskThree() throws Exception {
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务三完成");
+    }
+}
---
+++ src/main/java/com/inspur/bigdata/web/WebLogAspect.java
@@ -0,0 +1,47 @@
+/**
+ * @(#)WebLogAspect.java V1.2.0 16-9-6 
+ * Copyright (c) 2014-2016 The inspur Software Foundation.All rights reserved 
+ */
+package com.inspur.bigdata.web;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Chen Hao
+ * @version V1.2.0 16-9-6
+ */
+
+@Aspect
+@Component
+public class WebLogAspect {
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Pointcut("execution(public * com.didispace.web..*.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+    }
+
+}
